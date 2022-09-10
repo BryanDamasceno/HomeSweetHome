efcdab5c1e09633d74b314f3e13c13e4bcac3f8c
Bryan Damasceno <bryan42damasceno@gmail.com>
2022-09-10 19:52:05 -0300

    adding ffuf and silent logs

--- HomeSweetHome.sh
+++ HomeSweetHome.sh
@@ -5,7 +5,8 @@ checkCommand=$(which nmap)
 checkCommand="$checkCommand"
 
 if [ "$checkCommand" == "" ]; then
-        sudo apt-get install nmap
+        sudo apt-get install nmap >/dev/null
+        echo 'Nmap succesful installed!'
 
 else
         echo nmap already installed
@@ -16,12 +17,24 @@ checkCommand=$(which go)
 checkCommand="$checkCommand"
 
 if [ "$checkCommand" == "" ]; then
+        echo Removing previous go version...
         rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
-        curl -OL https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
-        sudo tar -C /usr/local -xvf go1.19.1.linux-amd64.tar.gz
-        echo "export PATH=$PATH:/usr/local/go/bin" >>~/.profile
+
+        echo Installing Go
+
+        curl -OL https://go.dev/dl/go1.19.1.linux-amd64.tar.gz >/dev/null
+        echo -ne '>>>>>>>>>>>>>>> 30%\r'
+
+        sudo tar -C /usr/local -xvf go1.19.1.linux-amd64.tar.gz >/dev/null
+        echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 70%\r'
+
+        echo "export PATH=$PATH:/usr/local/go/bin" >>~/.profile >/dev/null
+        echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 100%\r'
+
+        echo 'Go succesful installed!'
 
 else
+
         echo go already installed
 fi
 
@@ -30,9 +43,9 @@ checkCommand=$(which wafw00f)
 checkCommand="$checkCommand"
 
 if [ "$checkCommand" == "" ]; then
-        git clone git@github.com:EnableSecurity/wafw00f.git
+        git clone git@github.com:EnableSecurity/wafw00f.git >/dev/null
         cd wafw00f
-        python3 setup.py install
+        python3 setup.py install >/dev/null
 
 else
         echo wafw00f already installed
@@ -43,8 +56,21 @@ checkCommand=$(which gobuster)
 checkCommand="$checkCommand"
 
 if [ "$checkCommand" == "" ]; then
-        go install github.com/OJ/gobuster/v3@latest
+        go install github.com/OJ/gobuster/v3@latest >/dev/null
 
 else
         echo gobuster already installed
 fi
+
+#===============================FFUF===============================
+checkCommand=$(which ffuf)
+checkCommand="$checkCommand"
+
+if [ "$checkCommand" == "" ]; then
+        go install github.com/ffuf/ffuf@latest >/dev/null
+
+else
+        echo ffuf already installed
+fi
+
+cp ~/go/bin/* /usr/bin
